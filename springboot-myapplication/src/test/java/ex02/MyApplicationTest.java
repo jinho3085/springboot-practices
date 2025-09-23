8dc9e6f98b7b8e62ac15c2101ca3b887d56a991a
jinho3085 <jinho3085@naver.com>
2025-09-23 11:32:06 +0900

    springboot-myapplication

---
+++ springboot-myapplication/src/test/java/ex02/MyApplicationTest.java
@@ -0,0 +1,20 @@
+package ex02;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import ex02.component.MyComponent;
+
+
+@SpringBootTest
+public class MyApplicationTest {
+	
+	@Test
+	public void testMyComponetNotNull(ApplicationContext applicationContext) {
+		MyComponent myComponent = applicationContext.getBean("MyComponent", MyComponent.class);
+		assertNotNull(myComponent);
+	}
+}
